563dd38798c2355e1f50ed51513f4e13cfbd2b74
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-12-12 13:50:39 +0300

    Update ListContactsStyled.jsx

--- src/components/ListContacts/ListContactsStyled.jsx
+++ src/components/ListContacts/ListContactsStyled.jsx
@@ -4,3 +4,12 @@ export const List = styled.ul`
   padding-top: ${props => props.theme.space[3]}px;
   padding-left: ${props => props.theme.space[4]}px;
 `;
+
+export const ElementList = styled.li`
+  display: flex;
+  align-items: center;
+
+  &:not(:last-child) {
+    margin-bottom: ${props => props.theme.space[3]}px;
+  }
+`;
