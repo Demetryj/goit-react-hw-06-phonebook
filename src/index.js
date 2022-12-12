c393572486c2a75bee46623f27418349462dfed1
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-12-12 18:05:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ThemeProvider } from 'styled-components';
 import { store, persistor } from 'redux/store';
-import { theme } from './theme';
+import { theme, GlobalStyles } from 'styles';
 import { App } from 'components/App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider theme={theme}>
+          <GlobalStyles />
           <App />
         </ThemeProvider>
       </PersistGate>
