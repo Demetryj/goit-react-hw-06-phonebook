bd300b2094056735db8ce9714ffab358e49e3881
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-12-12 13:50:24 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,79 +1,32 @@
-import { useState, useEffect, useRef, useMemo } from 'react';
-import { nanoid } from 'nanoid';
-
+// import { useEffect, useRef } from 'react';
 import { Forma } from 'components/Forma/Forma';
 import { ListContacts } from 'components/ListContacts/ListContacts';
 import { Filter } from 'components/Filter/Filter';
 import { Box } from './Box';
 import { Title, TitleContacts } from './Titles/TitlesStyled';
 
-const LS_KEY = 'contacts';
+// const LS_KEY = 'contacts';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem(LS_KEY)) ?? '';
-  });
-  const [filter, setFilter] = useState('');
+  // const isFirstRender = useRef(true);
 
-  const isFirstRender = useRef(true);
+  // useEffect(() => {
+  //   if (isFirstRender.current) {
+  //     isFirstRender.current = false;
+  //     return;
+  //   }
 
-  useEffect(() => {
-    if (isFirstRender.current) {
-      isFirstRender.current = false;
-      return;
-    }
-
-    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContacts = dataContact => {
-    if (contacts.some(contact => contact.name === dataContact.name)) {
-      alert(`${dataContact.name} is already in contacts`);
-      return;
-    }
-
-    setContacts(prevContacts => [
-      ...prevContacts,
-      { ...dataContact, id: nanoid() },
-    ]);
-  };
-
-  const deleteContacts = id => {
-    setContacts(prevContacts =>
-      prevContacts.filter(contact => {
-        return contact.id !== id;
-      })
-    );
-  };
-
-  /* Поветає масив з тими контактами, які мають відмінний id від
-  id контакта, який видаляємою.
-  Тобно повертає масив з тими контактами які не схожі за id, з тим, 
-  який видаляємо. */
-
-  const changeFilter = event => {
-    setFilter(event.currentTarget.value);
-  };
-
-  const visibileContacts = useMemo(() => {
-    const normalyzeFilter = filter.toLowerCase();
-
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalyzeFilter)
-    );
-  }, [contacts, filter]);
+  //   localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  // }, [contacts]);
 
   return (
     <Box p={[4]}>
       <Title>Phonebook</Title>
-      <Forma onSubmit={addContacts} contacts={contacts} />
+      <Forma />
 
       <TitleContacts>Contacts</TitleContacts>
-      <Filter onChangeFilter={changeFilter} value={filter} />
-      <ListContacts
-        allContacts={visibileContacts}
-        onClickDelete={deleteContacts}
-      />
+      <Filter />
+      <ListContacts />
     </Box>
   );
 };
