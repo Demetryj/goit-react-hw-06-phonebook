f52451e47b57e57b0d5ebf18b1bf899a21a6c2cc
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-12-12 18:05:55 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,13 @@
 // import { useEffect, useRef } from 'react';
-import { Forma } from 'components/Forma/Forma';
-import { ListContacts } from 'components/ListContacts/ListContacts';
-import { Filter } from 'components/Filter/Filter';
+import { Forma } from 'components/Forma';
+import { ListContacts } from 'components/ListContacts';
+import { Filter } from 'components/Filter';
 import { Box } from './Box';
 import { Title, TitleContacts } from './Titles/TitlesStyled';
 
 // const LS_KEY = 'contacts';
 
 export const App = () => {
-  // const isFirstRender = useRef(true);
-
-  // useEffect(() => {
-  //   if (isFirstRender.current) {
-  //     isFirstRender.current = false;
-  //     return;
-  //   }
-
-  //   localStorage.setItem(LS_KEY, JSON.stringify(contacts));
-  // }, [contacts]);
-
   return (
     <Box p={[4]}>
       <Title>Phonebook</Title>
